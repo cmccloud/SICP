ad186920443d7e408c84a3058bf36766ca2570fc
Christopher McCloud <mccloud.christopher@gmail.com>
2015-05-23 11:58:02 -0700

    adds rational arithmetic

---
+++ chapter-2.scm
@@ -0,0 +1,23 @@
+;; Rational Arithmetic
+;; 2.1.1
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (subtract-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (multiply-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (divide-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equals-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
