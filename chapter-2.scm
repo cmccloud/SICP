dc5981575b5b66baf74202a053f09f8ef7c257e0
Christopher McCloud <mccloud.christopher@gmail.com>
2015-05-23 13:30:22 -0700

    adds exercise 2.3

--- chapter-2.scm
+++ chapter-2.scm
@@ -60,3 +60,82 @@
 
 (define (sign x)
   (if (>= x 0) 1 0))
+
+;; Exercise 2.2
+
+(define (make-segment x y)
+  (cons x y))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point point)
+  (car point))
+
+(define (y-point point)
+  (cdr point))
+
+(define (midpoint-segment s)
+  (let ((start (start-segment s))
+        (end (end-segment s)))
+    (make-point (/ (+ (x-point start) (x-point end))
+                   2)
+                (/ (+ (y-point start) (y-point end))
+                   2))))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+;; Exercise 2.3
+
+(define (length-segment s)
+  (let ((x (start-segment s))
+        (y (end-segment s)))
+    (abs (- (x-point x)
+            (x-point y)))))
+
+(define (height-segment s)
+  (let ((x (start-segment s))
+        (y (end-segment s)))
+    (abs (- (y-point x)
+            (y-point y)))))
+
+;; rectangle defined as two segments
+(define (make-rect s1 s2)
+  (cons s1 s2))
+
+(define (length-rect rect)
+  (length-segment (car rect)))
+
+(define (height-rect rect)
+  (height-segment (cdr rect)))
+
+(define (perimeter-rect rect)
+  (* 2 (+ (length-rect rect)
+          (height-rect rect))))
+
+(define (area-rect rect)
+  (* (length-rect rect)
+     (height-rect rect)))
+
+;; rectangle defined as three points, forming segments (a, b) (a, c)
+(define (make-rect a b c)
+  (cons (make-segment a b)
+        (make-segment a c)))
+
+;; rectangle defined as four points, forming segments (a, b) (c, d)
+(define (make-rect a b c d)
+  (cons (make-segment a b)
+        (make-segment c d)))
+
