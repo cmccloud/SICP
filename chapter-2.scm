ca689a89c5246948e46cdad99ac257358da86a0a
Christopher McCloud <mccloud.christopher@gmail.com>
2015-05-23 14:22:51 -0700

    adds 2.19

--- chapter-2.scm
+++ chapter-2.scm
@@ -278,3 +278,22 @@
                               state))))
   (reverse-helper x nil))
 
+;; Exercise 2.19
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else (+ (cc amount
+                     (except-first-denomination coin-values))
+                 (cc (- amount
+                        (first-denomiation coin-values))
+                     coin-values)))))
+
+(define (first-denomination x)
+  (car x))
+
+(define (except-first-denomination x)
+  (cdr x))
+
+(define (no-more? x)
+  (null? x))
+
