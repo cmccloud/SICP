b9fb00bd3473aa775f3df3a881b9582df5b2640e
Christopher McCloud <mccloud.christopher@gmail.com>
2015-05-23 17:56:54 -0700

    adds 2.30

--- chapter-2.scm
+++ chapter-2.scm
@@ -428,3 +428,19 @@
 
 (define (branch-structure branch)
   (cdr branch))
+
+;; Exercise 2.30
+
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (square tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+
+(define (square-tree-map tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree-map sub-tree)
+             (square sub-tree)))
+       tree))
+
