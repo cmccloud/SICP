69d15c98ca70348e531f536fa5f404a40c884c2c
Christopher McCloud <mccloud.christopher@gmail.com>
2015-05-27 13:19:23 -0700

    adds 2.58

--- chapter-2.scm
+++ chapter-2.scm
@@ -893,21 +893,13 @@
 
 (define (addend s) (cadr s))
 
-(define (augend s)
-  (let ((end (caddr s)))
-    (if (null? (cdr end))
-        end
-        (cons '+ (cddr s))))
+(define (augend s) (caddr s))
 
 (define (product? x) (and (pair? x) (eq? (car x) '*)))
 
 (define (multiplier p) (cadr p))
 
-(define (multiplicand p)
-  (let ((end (caddr p)))
-    (if (null? (cdr end))
-        end
-        (cons '* (cddr p)))))
+(define (multiplicand p) (caddr p))
 
 (define (deriv exp var)
   (cond ((number? exp) 0)
@@ -940,3 +932,51 @@
 (define (exponent exp) (caddr exp))
 
 (define (exponentiation? x) (and (pair? x) (eq? (car x) '**)))
+
+;; Exercise 2.57
+(define (augend s)
+  (if (null? (cdddr s))
+      (caddr s)
+      (cons '+ (cddr s))))
+
+(define (multiplicand p)
+  (if (null? (cdddr p))
+      (caddr p)
+      (cons '* (cddr p))))
+
+;; Exercise 2.58
+;; a.
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2))
+         (+ a1 a2))
+        (else (list a1 '+ a2))))
+
+(define (sum? x) (and (pair? x) (eq? (cadr x) '+)))
+
+(define (addend s) (car s))
+
+(define (augend s) (caddr s))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list m1 '* m2))))
+
+(define (product? s) (and (pair? s) (eq? (cadr s) '*)))
+
+(define (multiplier s) (car s))
+
+(define (multiplicand s) (caddr s))
+
+;; b.
+(define (augend s)
+  (let ((tail (cddr s)))
+    (if (null? (cdr tail)) (car tail) tail)))
+
+(define (multiplicand p)
+  (let ((tail (cddr p)))
+    (if (null? (cdr tail) (car tail) tail))))
