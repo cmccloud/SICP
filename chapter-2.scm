99795587274a4b85993537fb426937dd18989e89
Christopher McCloud <mccloud.christopher@gmail.com>
2015-05-29 12:02:46 -0700

    adds 2.60

--- chapter-2.scm
+++ chapter-2.scm
@@ -980,3 +980,30 @@
 (define (multiplicand p)
   (let ((tail (cddr p)))
     (if (null? (cdr tail) (car tail) tail))))
+
+;; Representing Sets
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set) set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1) (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+;; Exercise 2.59
+(define (union-set set1 set2)
+  (cond ((null? set2) set1)
+        ((null? set1) set2)
+        ((element-of-set? (car set1) set2)
+         (union-set (cdr set1) set2))
+        (else (union-set (cdr set1)
+                         (adjoin-set (car set1)
+                                     set2)))))
+
