19f3f10f94f72960d5431dfb96f3869ed9016a13
Christopher McCloud <mccloud.christopher@gmail.com>
2015-05-29 14:57:55 -0700

    adds 2.63

--- chapter-2.scm
+++ chapter-2.scm
@@ -1055,3 +1055,74 @@
                   (cons x1 (union-set (cdr set1) set2)))
                  (else
                   (cons x2 (union-set set1 (cdr set2)))))))))
+
+;; Sets as Binary Trees
+(define (entry tree) (car tree))
+
+(define (left-branch tree) (cadr tree))
+
+(define (right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set x (left-branch set)))
+        (else (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set)) (make-tree (entry set)
+                                      (adjoin-set x (left-branch set))
+                                      (right-branch set)))
+        (else (make-tree (entry set)
+                         (left-branch set)
+                         (adjoin-set x (right-branch set))))))
+
+;; Exercise 2.63
+(define (tree->list-1 tree)
+  (if (null? tree) '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result)
+    (if (null? tree) result
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree)
+                                          result)))))
+  (copy-to-list tree '()))
+
+;; tests
+(define a (make-tree 7
+                     (make-tree 3
+                                (make-tree 1 '() '())
+                                (make-tree 5 '() '()))
+                     (make-tree 9
+                                '()
+                                (make-tree 11 '() '()))))
+
+(define b (make-tree 3
+                     (make-tree 1 '() '())
+                     (make-tree 7
+                                (make-tree 5 '() '())
+                                (make-tree 9
+                                           '()
+                                           (make-tree 11 '() '())))))
+
+(define c (make-tree 5
+                     (make-tree 3
+                                (make-tree 1 '() '())
+                                '())
+                     (make-tree 9
+                                (make-tree 7 '() '())
+                                (make-tree 11 '() '()))))
+
+;; a. Same Results
+;; b. No. (tree->list-2 x) grows more slowly
