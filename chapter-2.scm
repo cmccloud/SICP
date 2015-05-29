78fc2831f4996e4e8410a241ebb6b214a90f172e
Christopher McCloud <mccloud.christopher@gmail.com>
2015-05-29 12:40:03 -0700

    adds 2.61

--- chapter-2.scm
+++ chapter-2.scm
@@ -1007,3 +1007,37 @@
                          (adjoin-set (car set1)
                                      set2)))))
 
+;; Exercise 2.60
+(define (adjoin-set x set) (cons x set))
+
+(define (union-set set1 set2)
+  (append set1 set2))
+
+;; Other procedures unchanged
+
+;; Sets as ordered lists
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2)) '()
+      (let ((x1 (car set1))
+            (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1 (intersection-set (cdr set1)
+                                          (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x2)
+               (intersection-set set1 (cdr set2)))))))
+
+;; Exercise 2.61
+(define (adjoin-set x set)
+  (cond ((null? set) (cons x set))
+        ((= x (car set)) set)
+        ((< x (car set)) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
