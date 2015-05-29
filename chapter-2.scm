f6c01bf876f0bdc63cd244bab00fa429404bf291
Christopher McCloud <mccloud.christopher@gmail.com>
2015-05-29 16:36:27 -0700

    refactors 2.66

--- chapter-2.scm
+++ chapter-2.scm
@@ -1291,10 +1291,12 @@
 
 ;; Exercise 2.66
 (define (lookup given-key set-of-records)
-  (cond ((null? set-of-records) false)
-        (else
-         (let ((entry-key (key (entry set-of-records))))
-           (cond ((= entry-key given-key) (entry set-of-records))
-                 ((< given-key entry-key)
-                  (lookup given-key (left-branch set-of-records)))
-                 (else (lookup given-key (right-branch set-of-records))))))))
+  (if ((null? set-of-records)
+       false
+       (let ((entry-key (key (entry set-of-records))))
+         (cond ((= entry-key given-key) (entry set-of-records))
+               ((< given-key entry-key)
+                (lookup given-key (left-branch set-of-records)))
+               (else (lookup given-key (right-branch set-of-records))))))))
+
+;; Huffman Encoding Trees
