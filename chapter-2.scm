80ec25c201b730c381e854db0381fcaf895614ca
Christopher McCloud <mccloud.christopher@gmail.com>
2015-05-30 14:09:06 -0700

    adds 2.73

--- chapter-2.scm
+++ chapter-2.scm
@@ -1458,3 +1458,259 @@
 ;; Exercise 2.72
 
 ;; Multiple Representations for Abstract Data
+
+;; Tagging procedures
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum: TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum: CONTENTS" datum)))
+
+;; Typed operations
+;; rectangular package
+(define (real-part-rectangular z) (car z))
+
+(define (imag-part-rectangular z) (cdr z))
+
+(define (magnitude-rectangular z)
+  (sqrt (+ (square (real-part-rectangular z))
+           (square (imag-part-rectangular z)))))
+
+(define (angle-rectangular z)
+  (atan (imag-part-rectangular z) (real-part-rectangular z)))
+
+(define (make-from-real-imag-rectangular x y)
+  (attach-tag 'rectangular (cons x y)))
+
+(define (make-from-mag-ang-rectangular r a)
+  (attach-tag 'rectangular
+              (cons (* r (cos a)) (* r (sin a)))))
+
+(define (rectangular? z)
+  (eq? (type-tag z) 'rectangular))
+
+;; polar package
+(define (real-part-polar z)
+  (* (magnitude-polar z) (cos (angle-polar z))))
+
+(define (imag-part-polar z)
+  (* (magnitude-polar z) (sin (angle-polar z))))
+
+(define (magnitude-polar z) (car z))
+
+(define (angle-polar z) (cdr z))
+
+(define (make-from-real-imag-polar x y)
+  (attach-tag 'polar
+              (cons (sqrt (+ (square x) (square y)))
+                    (attan y x))))
+
+(define (make-from-mag-ang-polar r a)
+  (attach-tag 'polar (cons r a)))
+
+(define (polar? z)
+  (eq? (type-tag z) 'polar))
+
+;; Generic operations
+(define (real-part z)
+  (cond ((rectangular? z)
+         (real-part-rectangular (contents z)))
+        ((polar? z)
+         (real-part-polar (contents z)))
+        (else (error "Unknown type: REAL-PART" z))))
+
+(define (imag-part z)
+  (cond ((rectangular? z)
+         (imag-part-rectangular (contents z)))
+        ((polar? z)
+         (imag-part-polar (contents z)))
+        (else (error "Unkown type: IMAG-PART" z))))
+
+(define (magnitude z)
+  (cond ((rectangular? z)
+         (magnitude-rectangular (contents z)))
+        ((polar? z)
+         (magnitude-polar (contents z)))
+        (else (error "Unknown type: MAGNITUDE" z))))
+
+(define (angle z)
+  (cond ((rectangular? z)
+         (angle-rectangular (contents z)))
+        ((polar? z)
+         (angle-polar (contents z)))
+        (else (error "Unknown type: ANGLE" z))))`
+
+(define (make-from-real-imag x y)
+  (make-from-real-imag-rectangular x y))
+
+(define (make-from-mag-ang r a)
+  (make-from-mag-ang-polar r a))
+
+;; higher order operations
+(define (add-complex z1 z2)
+  (make-from-real-imag (+ (real-part z1) (real-part z2))
+                       (+ (imag-part z1) (imag-part z2))))
+
+(define (sub-complex z1 z2)
+  (make-from-real-imag (- (real-part z1) (real-part z2))
+                       (- (imag-part z1) (imag-part z2))))
+
+(define (mul-complex z1 z2)
+  (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                     (* (angle z1) (angle z2))))
+
+(define (div-complex z1 z2)
+  (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                     (- (angle z1) (angle z2))))
+
+;; using data directed programming
+;; assumes global (put <op> <type> <item>) and
+;; global (get <op> <type>)
+
+;; rectangular package definition
+(define (install-rectangular-package)
+  ;; internal procedures
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (make-from-real-imag x y) (cons x y))
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+             (square (imag-part z)))))
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  (define (make-from-mag-ang r a)
+    (cons (* r (cos a)) (* r (sin a))))
+
+  ;; interface to rest of system
+  (define (tag x) (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+;; polar package definition
+(define (install-polar-package)
+  ;; internal procedures
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-mag-ang r a) (cons r a))
+  (define (real-part z)
+    (* (magnitude z) (cos (angle z))))
+  (define (imag-part z)
+    (* (magnitude z) (sin (angle z))))
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y))
+                (atan y x))))
+
+  ;; interface to rest of system
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+;; Generic operations
+(define (apply-generic op . args)
+  (let* ((type-tags (map type-tag args))
+        (proc (get op type-tags)))
+    (if proc
+        (apply proc (map contents args))
+        (error
+         "No method for these types: APPLY-GENERIC"
+         (list op type-tags)))))
+
+(define (real-part z) (apply-generic 'real-part z))
+
+(define (imag-part z) (apply-generic 'imag-part z))
+
+(define (magnitude z) (apply-generic 'magnitude z))
+
+(define (angle z) (apply-generic 'angle z))
+
+(define (make-from-real-imag x y)
+  ((get 'make-from-real-imag 'rectangular) x y))
+
+(define (make-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'polar) r a))
+
+;; Exercise 2.3.2
+(define (operator exp) (car exp))
+
+(define (operands exp) (cdr exp))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        (else ((get 'deriv (operator exp))
+               (operands exp) var))))
+
+;; a. Neither numbers nor variables are valid arguments for procedures
+;; 'operator' and 'operands', on which our method retrieval system depends
+
+;; b.
+(define (install-sum-deriv-package)
+  ;; internal procedures
+  (define (addend x) (car x))
+  (define (augend x) (cadr x))
+  (define (deriv operands var)
+    (make-sum (deriv (addend operands) var)
+              (deriv (augend operands) var)))
+
+  ;; interface to rest of system
+  (put 'deriv '(+) deriv)
+  'done)
+
+(define (install-product-deriv-package)
+  ;; internal procedures
+  (define (multiplier x) (car x))
+  (define (multiplicand x) (cadr x))
+  (define (deriv operands var)
+    (make-sum (make-product
+               (multiplier operands)
+               (deriv (multiplicand operands) var))
+              (make-product
+               (deriv (multiplier operands) var)
+               (multiplicand operands))))
+
+  ;; interface to rest of system
+  (put 'deriv '(*) deriv)
+  'done)
+
+;; c.
+(define (install-exp-deriv-package)
+  ;; internal procedures
+  (define (base x) (car x))
+  (define (exponent x) (cadr x))
+  (define (deriv operands var)
+    (make-product
+     (make-product (exponent operands)
+                   (make-exponentiation (base operands)
+                                        (make-sum (exponent operands) -1)))
+     (deriv (base operands) var)))
+
+  ;; interface to rest of system
+  (put 'deriv '(**) deriv)
+  'done)
+
+;; d.
+;; in install exp: (put 'deriv '(**) deriv) -> (put '(**) 'deriv deriv)
+;; in install product: (put 'deriv '(*) deriv) -> (put '(*) 'deriv deriv)
+;; in install sum: (put 'deriv '(+) deriv) -> (put '(+) 'deriv deriv)
