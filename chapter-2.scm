f2ba8b9bfc33795dc65a25ca63719f2bc8740ede
Christopher McCloud <mccloud.christopher@gmail.com>
2015-05-30 18:30:55 -0700

    adds 2.81

--- chapter-2.scm
+++ chapter-2.scm
@@ -2042,3 +2042,70 @@
 ;; within complex numbers package
 (define (=zero? x) (= (magnitude x) 0))
 (put '=zero? '(complex) =zero?)
+
+;; Coercion and Generic Procedures
+(define (scheme-number->complex n)
+  (make-complex-from-real-imag (contents n) 0))
+
+(put-coercion 'scheme-number
+              'complex
+              scheme-number->complex)
+
+(define (apply-generic op . args)
+  (let* ((type-tag (map type-tag args))
+         (proc (get op type-tags)))
+    (if proc
+        (apply proc (map contents args))
+        (if (= (length args) 2)
+            (let* ((type1 (car type-tags))
+                   (type2 (cadr type-tags))
+                   (a1 (car args))
+                   (a2 (cadr args))
+                   (t1->t2 (get-coercion type1 type2))
+                   (t2->t1 (get-coercion type2 type1)))
+              (cond (t1->t2
+                     (apply-generic op (t1->t2 a1) a2))
+                    (t2->t1
+                     (apply-generic op a1 (t2->t1 a2)))
+                    (else (error "No method for these types"
+                                 (list op type-tags)))))
+            (error "No method for these types"
+                   (list op type-tags))))))
+
+;; Exercise 2.81
+;; a. With Louis's changes to apply-generic, calling exp with two complex numbers would
+;; result in an infinite loop.
+;; b. Louis is correct, in a sense. Apply-generic should guard against the possibility that a programmer
+;; introduces a type coercion which hangs the system.
+;; c.
+(define (apply-generic op . args)
+  ;; internal procedures
+  (define (valid-length args)
+    (= (length args) 2))
+  (define (same-type args)
+    (= (type-tag (car args))
+       (type-tag (cadr args))))
+  (define (try-coercion tags op args)
+    (let* ((type1 (car tags))
+           (type2 (cadr tags))
+           (a1 (car args))
+           (a2 (cadr args))
+           (t1->t2 (get-coercion type1 type2))
+           (t2->t1 (get-coercion type2 type1)))
+      (cond (t1->t2
+             (apply-generic op (t1->t2 a1) a2))
+            (t2->t1
+             (apply-generic op a1 (t2->t1 a2)))
+            (else (error "No method for these types"
+                         (list op tags))))))
+  ;; procedure body
+  (let* ((type-tag (map type-tag args))
+         (proc (get op type-tags)))
+    (cond (proc (apply proc (map contents args)))
+          ((and (valid-length args)
+                (not (same-type args)))
+           (try-coercion type-tag op args))
+          (else (error "No method for these types"
+                       (list op type-tags))))))
+
+
