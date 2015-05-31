2666388c462d17d8ef69b0b81c78b2575384180d
Christopher McCloud <mccloud.christopher@gmail.com>
2015-05-31 15:13:42 -0700

    adds 3.2

--- chapter-3.scm
+++ chapter-3.scm
@@ -18,3 +18,27 @@
   (define C2 (acc-2 5))
   (and (equal? '(15 25 35) (list A B C))
        (equal? '(10 15 20) (list A2 B2 C2))))
+
+;; Exercise 3.2
+(define (make-monitored fn)
+  (let ((count 0))
+    (define (monitor x)
+      (if (eq? x 'how-many-calls?)
+          count
+          (begin (set! count (+ count 1))
+                 (fn x))))
+    monitor))
+
+;; tests
+(define (test3-2)
+  (define (inc x) (+ x 1))
+  ;; make monitored should track the number of calls
+  (define m-inc (make-monitored inc))
+  (let* ((one (m-inc 0))
+         (two (m-inc 1))
+         (five (m-inc 4))
+         (count-three (m-inc 'how-many-calls?))
+         (four (m-inc 3))
+         (count-four (m-inc 'how-many-calls?)))
+    (equal? '(1 2 5 3 4 4)
+            (list one two five count-three four count-four))))
