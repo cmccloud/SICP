167f71e56f54cd6a80d830d5489c7a4252639e04
Christopher McCloud <mccloud.christopher@gmail.com>
2015-06-04 16:21:28 -0700

    adds event loop code, propagation cycle

--- chapter-3.scm
+++ chapter-3.scm
@@ -1076,7 +1076,7 @@
         (else (error "Invalid signal" (list s1 s2)))))
 
 (define (inverter input output)
-  (define (invert-intput)
+  (define (invert-input)
     (let ((new-value (logical-not (get-signal input))))
       (after-delay inverter-delay
                    (lambda () (set-signal! output new-value)))))
@@ -1132,7 +1132,6 @@
 ;; Initial State
 ;; a1 = 0, a2 = 0
 ;; b1 = 1, b2 = 1
-
 ;; a1 -> 1
 ;; b1 -> 0
 ;; (and-gate b1 b2) -> (and-gate 0 1) -> 0
@@ -1159,32 +1158,130 @@
 ;;         (max (+ and-gate-delay inverter-delay)
 ;;              or-gate-delay)))
 
-(define (call-each procedures)
-  (if (null? procedures) 'done
-      (begin ((car procedures))
-             (call-each (cdr procedures)))))
+;; Representing Wires
+;; helpers
+(define call-each (partial for-each (lambda (p) (p))))
 
+;; wire definition
 (define (make-wire)
   (let ((signal-value 0) (action-procedures '()))
     (define (set-my-signal! new-value)
       (if (not (= signal-value new-value))
           (begin (set! signal-value new-value)
-                 (call-each action-procedures))
-          'done))
+                 (call-each action-procedures)
+                 'done)))
     (define (accept-action-procedure! proc)
       (set! action-procedures
-            (cons proc action-procedures)
-            (proc)))
+            (cons proc action-procedures))
+            (proc))
     (define (dispatch m)
       (cond ((eq? m 'get-signal) signal-value)
             ((eq? m 'set-signal!) set-my-signal!)
             ((eq? m 'add-action!) accept-action-procedure!)
             (else (error "Unknown operation: WIRE" m))))
 
-    dispach))
+    dispatch))
 
+;; handlers
 (define (get-signal wire) (wire 'get-signal))
 (define (set-signal! wire new-value)
   ((wire 'set-signal!) new-value))
 (define (add-action! wire action-procedure)
   ((wire 'add-action!) action-procedure))
+
+;; Agenda Definition and specification
+;; (make-agenda) returns a new empty agenda
+;; (empty-agenda? agenda) -> bool
+;; (first-agenda-item agenda) -> returns first item in agenda
+;; (remove-first-agenda-item! agenda) -> modifies the agenda by removing the
+;; first item
+;; (add-to-agenda! time action agenda) -> modifies the agenda by adding the
+;; given action procedure to be run at the specified time
+;; (current-time agenda) -> returns the current simulation time
+(define the-agenda (make-agenda))
+
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+                  action
+                  the-agenda))
+
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+        (first-item)
+        (remove-first-agenda-item! the-agenda)
+        (propagate))))
+;; Exercise 3.31 - Diagram
+
+;; Implementing the Agenda
+
+;; Time Segments :: (time, queue)
+(define (make-time-segment time queue)
+  (cons time queue))
+
+(define (segment-time segment)
+  (car segment))
+
+(define (segment-queue segment)
+  (cdr segment))
+
+;; Agenda - One dimensional table of time segments, with time at head
+;; (time, segments)
+(define (make-agenda) (list 0))
+(define (current-time agenda) (car agenda))
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+(define (segments agenda) (cdr agenda))
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+(define (first-segment agenda)
+  (car (segments agenda)))
+(define (rest-segments agenda)
+  (cdr (segments agenda)))
+;; An agenda is empty if it has no time segments
+(define (empty-agenda?)
+  (null? (segments agenda)))
+
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+    (or (null? segments)
+        (< time (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+        (insert-queue! (segment-queue (car segments))
+                       action)
+        (let ((rest (cdr segments)))
+          (if (belongs-before? rest)
+              (set-cdr!
+               segments
+               (cons (make-new-time-segment time action)
+                     rest))
+              (add-to-segments! rest)))))
+  (let ((segments (segments agenda)))
+    (if (belongs-before? segments)
+        (set-segments!
+         agenda
+         (cons (make-new-time-segment time action)
+               segments))
+        (add-to-segments! segments))))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+    (delete-queue! q)
+    (if (empty-queue? q)
+        (set-segments! agenda (rest-segments agenda)))))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (error "Agenda is empty: FIRST-AGENDA-ITEM")
+      (let ((first-seg (first-segment agenda)))
+        (set-current-time! agenda
+                           (segment-time first-seg))
+        (front-queue (segment-queue first-seg)))))
+
+;; Exercise 3.32 - TODO
