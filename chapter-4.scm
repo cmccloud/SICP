7332334d0a4b3254135d052713969e5fc6456fee
Christopher McCloud <mccloud.christopher@gmail.com>
2015-06-09 10:01:19 -0700

    adds 4.2

--- chapter-4.scm
+++ chapter-4.scm
@@ -206,3 +206,14 @@
             (make-if (cond-predicate first)
                      (sequence->exp (cond-actions first))
                      (expand-clauses rest))))))
+
+;; Exercise 4.2
+;; a - The evaluator will attempt to apply the procedure define to x 3
+;; The particulars of what define could evaluate to in the given environment
+;; aside, we know by definition that x will always be undefined - given that
+;; it could never have been associated in any environment.
+
+;; b
+(define (_application? exp) (tagged-list? exp 'call))
+(define (_operator exp) (cadr exp))
+(define (_operands exp) (cddr exp))
