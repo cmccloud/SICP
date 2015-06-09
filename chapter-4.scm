169825544664d01bbadd05da644f0493bfb2539e
Christopher McCloud <mccloud.christopher@gmail.com>
2015-06-09 15:26:22 -0700

    adds 4.6

--- chapter-4.scm
+++ chapter-4.scm
@@ -538,3 +538,21 @@
    (list
     (list 'eval 'cond (lambda (exp env) (eval (cond->if exp) env)))))
   'cond-package-installed!)
+
+(define (let-package install-proc eval)
+  ;; Internal definitions
+  (define (let-bindings exp) (cadr exp))
+  (define (let-body exp) (caddr exp))
+  (define (binding-var binding) (car binding))
+  (define (binding-exp binding) (cadr binding))
+  (define (let->combination exp)
+    (list (make-lambda
+           (map binding-var (let-bindings exp))
+           (let-body exp))
+          (map binding-exp (let-bindings exp))))
+
+  ;; Interface
+  (install-proc
+   (list
+    (list 'eval 'let (lambda (exp env) (eval (let->combination exp) env)))))
+  'let-package-installed!)
