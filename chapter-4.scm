8a74a643d1b8b0b0257e0a8e681615b5f2db3742
Christopher McCloud <mccloud.christopher@gmail.com>
2015-06-11 10:18:17 -0700

    changes 4.5

--- chapter-4.scm
+++ chapter-4.scm
@@ -516,40 +516,41 @@
 
   (define (make-begin seq) (cons 'begin seq))
 
-  (define (valid-recipient? exp)
-    (and (pair? exp)
-         (pair? (cadr exp)) ;; check for variadic lambda
-         (= (length (cadr exp)) 1))) ;; check for arity
+  (define (valid-recipient? exp env)
+    (let ((recipient (eval exp env)))
+      (and (lambda? recipient) ;; must be a function
+           (pair? (cadr recipient)) ;; check for variadic lambda
+           (= (length (cadr recipient)) 1))) ;; check for arity
 
   (define (else->exp first rest clauses)
     (if (null? rest)
         (sequence->exp (cond-actions first))
         (error "Else clause isn't last: COND->IF" clauses)))
 
-  (define (arrow->if first rest)
-    (if (valid-recipient? (caddr first))
+  (define (arrow->if first rest env)
+    (if (valid-recipient? (caddr first) env)
         (make-if (cond-predicate first)
                  (list (cadr (cond-actions first))
                        (cond-predicate first))
                  (expand-clauses rest))
         (error "Recipient must be single arity procedure: COND-IF" (caddr first))))
 
-  (define (expand-clauses clauses)
+  (define (expand-clauses clauses env)
     (if (null? clauses) 'false
         (let ((first (car clauses))
               (rest (cdr clauses)))
           (cond ((cond-else-clause? first) (else->exp first rest clauses))
-                ((arrow-clause? first) (arrow->if first rest))
+                ((arrow-clause? first) (arrow->if first rest env))
                 (else (make-if (cond-predicate first)
                                (sequence->exp (cond-actions first))
                                (expand-clauses rest)))))))
 
-  (define (cond->if exp)
-    (expand-clauses (cond-clauses exp)))
+  (define (cond->if exp env)
+    (expand-clauses (cond-clauses exp env)))
   ;; Interface
   (install-proc
    (list
-    (list 'eval 'cond (lambda (exp env) (eval (cond->if exp) env)))))
+    (list 'eval 'cond (lambda (exp env) (eval (cond->if exp env) env)))))
   'extended-cond-package-installed!)
 
 (define (let-package install-proc eval)
