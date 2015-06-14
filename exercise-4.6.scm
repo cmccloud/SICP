736349099b5ec0f7e9be9d24ace50207523bf7e8
Christopher McCloud <mccloud.christopher@gmail.com>
2015-06-14 14:11:18 -0700

    fixes 4.6 and adds tests

--- exercise-4.6.scm
+++ exercise-4.6.scm
@@ -3,14 +3,20 @@
 (define (let-package install-proc eval)
   ;; Internal definitions
   (define (let-bindings exp) (cadr exp))
-  (define (let-body exp) (caddr exp))
+  (define (let-body exp) (cddr exp))
+  (define (binding-exps bindings)
+    (if (null? bindings)
+        '()
+        (cons (cadar bindings)
+              (binding-exps (cdr bindings)))))
   (define (binding-var binding) (car binding))
-  (define (binding-exp binding) (cadr binding))
+  (define (make-lambda parameters body)
+    (cons 'lambda (cons parameters body)))
   (define (let->combination exp)
-    (list (make-lambda
+    (cons (make-lambda
            (map binding-var (let-bindings exp))
            (let-body exp))
-          (map binding-exp (let-bindings exp))))
+          (binding-exps (let-bindings exp))))
 
   ;; Interface
   (install-proc
@@ -18,3 +24,13 @@
     (list 'eval 'let (lambda (exp env) (eval (let->combination exp) env)))))
   'let-package-installed!)
 
+;; tests
+(define (test4-6)
+  (define expression
+    '(let ((x 3) (y 4) (z 5))
+       (* x y z)))
+  (define target
+    '((lambda (x y z)
+        (* x y z)) 3 4 5))
+  (define result (let->combination expression))
+  (equal? result target))
