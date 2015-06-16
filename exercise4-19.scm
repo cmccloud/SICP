fa48a796938bb23e0ce53d56d7805c508653c28d
Christopher McCloud <mccloud.christopher@gmail.com>
2015-06-16 16:43:00 -0700

    adds 4.19

---
+++ exercise4-19.scm
@@ -0,0 +1,6 @@
+;; Exercise 4.19
+;; I agree with Alyssa, given the model we've been using thus far.
+;; I would agree with Eva if I could think of a clever way to
+;; make procedure evaluation simultaneous, but doing so seems
+;; overly cumbersome for the benefits it brings. I would rather
+;; have an error than an incorrect value.
