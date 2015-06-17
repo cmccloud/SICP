e436f50c236a629f1b9df5f3496ca27c870de86b
Christopher McCloud <mccloud.christopher@gmail.com>
2015-06-16 17:37:42 -0700

    adds part b to 4.20

--- exercise-4.20.scm
+++ exercise-4.20.scm
@@ -31,3 +31,16 @@
        (set! fact (lambda (n) (if (= n 1) 1 (* n (fact (- n 1))))))
        (fact 10)))
   (equal? (transform-letrec expression) target))
+
+;; b - Louis's definition
+(let ((even? (lambda (n) (if (= n 0) true (odd? (- n 1)))))
+      (odd? (lambda (n) (if (= n 0) false (even? (- n 1))))))
+  (even? 10))
+;; which reduces to
+((lambda (even? odd?)
+   (even? 10))
+ (lambda (n) (if (= n 0) true (odd? (- n 1))))
+ (lambda (n) (if (= n 0) false (even? (- n 1)))))
+;; Because the bodies of even and odd are defined outside of the
+;; scope of the lambda which represents our let form, their
+;; internal calls to each other have no referents.
